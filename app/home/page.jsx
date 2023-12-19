5cd294feb01a42258bec72c37825329ae19a646e
Jateen Sunder <jateen.sunder@techchefz.com>
2023-12-19 13:20:02 +0530

    creating future component

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LandingBanner from "@/components/home/landingBanner";
 import styles from "./style.module.scss";
 import LandingPlayer from "@/components/home/landingPlayer";
+import WeAreFuture from "@/components/home/weAreFuture";
 
 const HomePage = () => {
   return (
@@ -12,6 +13,10 @@ const HomePage = () => {
       
       <section>
         <LandingPlayer />
+      </section>
+
+      <section className={styles.sectionSpace}>
+        <WeAreFuture />
       </section>
     </div>
   );
--- components/home/weAreFuture/index.jsx
+++ components/home/weAreFuture/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { ImageCustom } from "@/components/ui/imageCustom";
 
 const WeAreFuture = () => {
-  return <div className={styles.futureStyle}></div>;
+  return (
+    <>
+      <div className={styles.futureTitle}>
+        <p className={styles.text}>We are a</p>
+        <h2 className={styles.title}>
+          Pioneering <br /> Future{" "}
+          <ImageCustom src="vercel.svg" width="60" height="60" alt="" /> Global{" "}
+          <br />
+          <span className={styles.titleHighlight}>Technology Company.</span>
+        </h2>
+      </div>
+    </>
+  );
 };
 
 export default WeAreFuture;
