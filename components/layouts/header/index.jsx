c338ad7b018069d55d2f3db01fcaa3deb0dc93e6
unknown <alok.kumar@techchefz.com>
2023-12-19 15:22:14 +0530

    working header component

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from "./style.module.scss";
+import Link from 'next/link';
+import { Button } from 'bootstrap';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className={styles.headerMain}>
+      <div className={styles.logo}></div>
+      <nav>
+        <ul>
+          <li><Link href="/">About Us</Link></li>
+          <li><Link href="/">Solutions</Link></li>
+          <li><Link href="/">Technology</Link></li>
+          <li><Link href="/">Portfolio</Link></li>
+          <li><Link href="/">More</Link></li>
+        </ul>
+      </nav>
+      <div className={styles.headerRight}>
+        <div className={styles.modeBtn}></div>
+        <Button variant="blueBtn" size="md">Estimate Project</Button>
+      </div>
+    </header>
   )
 }
 
