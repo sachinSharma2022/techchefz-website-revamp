1c4c10aa70788b99b0e8d7610c902b82442beec9
Sachin Sharma <sachin.sharma@techchefz.com>
2023-12-20 11:13:29 +0530

    FIX: redirection

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
-import AboutPage from "./about/page";
 import HomePage from "./home/page";
 
 export default function Home() {
-  return (
-   <div>
-      {/* <HomePage /> */}
-      <AboutPage/>
-   </div>
-  )
+  return <HomePage />;
 }
