ba93fd21b352a87c226e0ac43e2b91c7b99428a2
unknown <alok.kumar@techchefz.com>
2023-12-22 11:04:44 +0530

    working on footer component

--- components/layouts/footer/index.jsx
+++ components/layouts/footer/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { ImageCustom } from "@/components/ui/imageCustom";
 
 const Footer = () => {
-  return <footer className={styles.footerSection}>Footer</footer>;
+  return (
+    <footer className={styles.footerSection}>
+      <div className="row">
+        <div className="col-md-5">
+          <div className={styles.footlogo}>
+            <ImageCustom src="/images/logo-white.svg" width={143} height={48} alt="" />
+          </div>
+          <p>Stay at the forefront of innovation, trends, and industry insights by subscribing to our newsletter. </p>
+        </div>
+        <div className="col-md-7"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
-import { useState } from "react";
-import { Switch } from "@headlessui/react";
+import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ImageCustom } from "@/components/ui/imageCustom";
 import styles from "./style.module.scss";
 
 const Header = () => {
-  const [enabled, setEnabled] = useState(false);
   return (
     <header className={styles.headerMain}>
       <div className={styles.logo}>
-        <ImageCustom src="/images/logo.svg" width={153} height={40} alt="" />
+        <Link href="/">
+          <ImageCustom src="/images/logo.svg" width={153} height={40} alt="" />
+        </Link>
       </div>
       <nav>
         <ul>
           <li>
-            <Link href="/">About Us</Link>
+            <Link href="/about">About Us</Link>
           </li>
           <li>
             <Link href="/">Solutions</Link>
@@ -34,18 +34,15 @@ const Header = () => {
         </ul>
       </nav>
       <div className={styles.headerRight}>
-        <div className={styles.modeBtn}>
-          <Switch
-            checked={enabled}
-            onChange={setEnabled}
-            className={`${enabled ? "bg-teal-900" : "bg-teal-700"}`}
-          >
-            <span className="sr-only">Use setting</span>
-            <span
-              aria-hidden="true"
-              className={`${enabled ? "translate-x-9" : "translate-x-0"}`}
+        <div className="modeBtn">
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
             />
-          </Switch>
+          </div>
         </div>
         <Button variant="blueBtn" size="sm">
           Estimate Project
