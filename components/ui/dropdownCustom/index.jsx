773f58411c3cd9471622cde5100efaefe21a7e73
Jateen Sunder <jateen.sunder@techchefz.com>
2023-12-23 20:16:38 +0530

    fix fonts size weight and spacing and other

--- components/ui/dropdownCustom/index.jsx
+++ components/ui/dropdownCustom/index.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./style.module.scss";
-import { ImageCustom } from "../imageCustom";
 
 const DropdownCustom = () => {
   return (
     <div className={styles.dropdownContainerStyle}>
       <div className="form-floating">
         <div class="input-group">
-          <div class="input-group-prepend"></div>
+          <div class="input-group-prepend">
+            <select
+              className={`${styles.formSelect} form-select`}
+              id="floatingSelectGrid"
+              aria-label="Floating label select example"
+            >
+              <option selected>IN</option>
+              <option value="1">EN</option>
+              <option value="2">EU</option>
+            </select>
+          </div>
 
           <input
             type="text"
