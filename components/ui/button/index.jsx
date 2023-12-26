35f0fddeaeab25c5e972cd12eff3d97d915b35c0
unknown <alok.kumar@techchefz.com>
2023-12-26 12:32:42 +0530

    working on footer

--- components/ui/button/index.jsx
+++ components/ui/button/index.jsx
@@ -9,11 +9,13 @@ const buttonVariants = cva(styles.base, {
     variant: {
       default: styles.variant_default,
       blueBtn: styles.variant_blueBtn,
+      ligthBlueBtn: styles.variant_ligthBlueBtn,
       outline: styles.variant_outline,
     },
     size: {
       default: styles.size_default,
       md: styles.size_md,
+      lg: styles.size_lg,
       sm: styles.size_sm,
       xs: styles.size_xs,
     },
