052822cf850c144cff165860b5e9311dbd308723
Jateen Sunder <jateen.sunder@techchefz.com>
2023-12-28 13:36:42 +0530

    working on portfolio page

--- components/ui/primaryDropdown/index.jsx
+++ components/ui/primaryDropdown/index.jsx
@@ -4,15 +4,21 @@ import styles from "./style.module.scss";
 const PrimaryDropdown = () => {
   return (
     <div className={styles.dropdownContainerStyle}>
-     <div class="form-floating">
-  <select className={`${styles.selectMain} form-select`} id="floatingSelect" aria-label="Floating label select example">
-    <option selected>Open this select menu</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </select>
-  <label className={styles.customLabel} for="floatingSelect">Works with selects</label>
-</div>
+      <div class="form-floating">
+        <select
+          className={`${styles.selectMain} form-select`}
+          id="floatingSelect"
+          aria-label="Floating label select example"
+        >
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <label className={styles.customLabel} for="floatingSelect">
+          Works with selects
+        </label>
+      </div>
     </div>
   );
 };
