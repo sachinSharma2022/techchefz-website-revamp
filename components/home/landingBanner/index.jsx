5f16195d09261a3ea583ff47572c8757d3420d01
harshth11 <harsh@techchefz.com>
2024-01-05 00:42:53 +0530

    hero section text transition improved

--- components/home/landingBanner/index.jsx
+++ components/home/landingBanner/index.jsx
@@ -1,25 +1,39 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
 import { Button } from "../../ui/button";
 import { Icons } from "@/components/icons";
 import styles from "./style.module.scss";
 
+const texts = ["Through Data", "Through Connectivity", "Through Data2"];
 const LandingBanner = () => {
+  const [textIndex, setTextIndex] = useState(0);
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <section className={`${styles.landingBannerStyle}`}>
       <h1 className={styles.title}>
         Humanizing <div /> Digital Experience <div />
-        <div className={styles.titleHighlight} id={styles.flip}>
-          <div>
-            <div className={styles.rollingTitle}>Through Data</div>
-          </div>
-          <div>
-            <div className={styles.rollingTitle}>abcdfds</div>
-          </div>
-          <div>
-            <div className={styles.rollingTitle}>Through Technology</div>
-          </div>
-        </div>
+        <ReactTextTransition
+       springConfig={{ stiffness: 100, damping: 50 }}
+        delay={300}
+        inline>
+        <div className={styles.rollingTitle}>{texts[textIndex]}</div></ReactTextTransition>
       </h1>
 
       <Button variant="blueBtn" size="md" className={styles.buttonContent}>
