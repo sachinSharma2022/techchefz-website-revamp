fa54fd0bdc959e7903deb82870c720fcd0c1ef5c
harshth11 <harsh@techchefz.com>
2024-01-05 20:35:12 +0530

    raect-transition package remooved

--- components/home/landingBanner/index.jsx
+++ components/home/landingBanner/index.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
-import ReactTextTransition from "react-text-transition";
 // import { Button } from "../../ui/button";
 import { Icons } from "@/components/icons";
 import styles from "./style.module.scss";
