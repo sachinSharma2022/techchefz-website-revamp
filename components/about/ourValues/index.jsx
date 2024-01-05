39d02b24099170707e056ec0084aed72144a853c
Jateen Sunder <jateen.sunder@techchefz.com>
2024-01-05 13:28:52 +0530

    fix: ui fixes

--- components/about/ourValues/index.jsx
+++ components/about/ourValues/index.jsx
@@ -50,67 +50,64 @@ const OurValues = () => {
   ];
   return (
     <section className={styles.ourValues}>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-7 col-lg-8">
-            <div className={styles.ourValuesHead}>
-              <h6 className={styles.ourValuesTitle}>Our Values</h6>
-              <h3 className={styles.ourValuesHeading}>
-                Our values are part of everything we create at Instrument.
-              </h3>
-            </div>
+      <div className="row">
+        <div className="col-md-7 col-lg-8">
+          <div className={styles.ourValuesHead}>
+            <h6 className={styles.ourValuesTitle}>Our Values</h6>
+            <h3 className={styles.ourValuesHeading}>
+              Our values are part of everything we create at Instrument.
+            </h3>
           </div>
         </div>
+      </div>
 
-        <div className={styles.ourValuesContent}>
-          <div className="row justify-content-between">
-            <div className="col-md-6 col-lg-6">
-              <div className={styles.ourValueImg}>
-                <ImageCustom
-                  src={"/images/img/ourValues.png"}
-                  width={600}
-                  height={550}
-                  alt="content-img"
-                />
-              </div>
+      <div className={styles.ourValuesContent}>
+        <div className="row justify-content-between">
+          <div className="col-md-6 col-lg-6">
+            <div className={styles.ourValueImg}>
+              <ImageCustom
+                src={"/images/img/ourValues.png"}
+                width={600}
+                height={550}
+                alt="content-img"
+              />
             </div>
-            <div className="col-md-6 col-lg-5">
-              <p className={styles.ourValuesText}>
-                We believe in a customer-centric ethic without and
-                people-centric paradigm within. With a strong sense of
-                community, ownership, and collaboration our people work in a
-                spirit of co-creation, co-innovation, and co-development to
-                engineer next-generation software products with the help of
-                accelerators.
-              </p>
-              <div className={styles.ourValuesAccordiaon}>
-                {accordianData.map((data, index) => (
-                  <Disclosure key={index}>
-                    {(panel) => {
-                      const { open, close } = panel;
-                      return (
-                        <>
-                          <Disclosure.Button
-                            className={styles.accordiaonHead}
-                            onClick={() => {
-                              if (!open) {
-                                close();
-                              }
-                              togglePanels({ ...panel, key: index });
-                            }}
-                          >
-                            {data.title}
-                            {!open ? <Icons.ArrowDown /> : <Icons.ArrowUp />}
-                          </Disclosure.Button>
-                          <Disclosure.Panel className={styles.accordiaonBody}>
-                            {data.content}
-                          </Disclosure.Panel>
-                        </>
-                      );
-                    }}
-                  </Disclosure>
-                ))}
-              </div>
+          </div>
+          <div className="col-md-6 col-lg-5">
+            <p className={styles.ourValuesText}>
+              We believe in a customer-centric ethic without and people-centric
+              paradigm within. With a strong sense of community, ownership, and
+              collaboration our people work in a spirit of co-creation,
+              co-innovation, and co-development to engineer next-generation
+              software products with the help of accelerators.
+            </p>
+            <div className={styles.ourValuesAccordiaon}>
+              {accordianData.map((data, index) => (
+                <Disclosure key={index}>
+                  {(panel) => {
+                    const { open, close } = panel;
+                    return (
+                      <>
+                        <Disclosure.Button
+                          className={styles.accordiaonHead}
+                          onClick={() => {
+                            if (!open) {
+                              close();
+                            }
+                            togglePanels({ ...panel, key: index });
+                          }}
+                        >
+                          {data.title}
+                          {!open ? <Icons.ArrowDown /> : <Icons.ArrowUp />}
+                        </Disclosure.Button>
+                        <Disclosure.Panel className={styles.accordiaonBody}>
+                          {data.content}
+                        </Disclosure.Panel>
+                      </>
+                    );
+                  }}
+                </Disclosure>
+              ))}
             </div>
           </div>
         </div>
