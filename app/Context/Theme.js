5880bf0060b8bef5f698fc9913d7cfce248c76ff
unknown <alok.kumar@techchefz.com>
2024-01-08 10:55:30 +0530

    working on dark mode

---
+++ app/Context/Theme.js
@@ -0,0 +1,14 @@
+"use client";
+import { createContext } from "react";
+import { useState } from "react";
+
+export const MyContext = createContext(null);
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+  return (
+    <MyContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </MyContext.Provider>
+  );
+}
