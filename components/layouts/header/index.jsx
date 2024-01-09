264a7dac8caff416bde97073631acac6d4d24680
harshth11 <harsh@techchefz.com>
2024-01-09 20:26:13 +0530

    UI development of Navbar Dropdown of technolgy section is done

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -22,6 +22,37 @@ const Header = () => {
   };
 
   const renderTechnologyMenu = () => {
+    const multiredirection = [
+      {
+        title: "CMS",
+        href: "#",
+      },
+      {
+        title: "Commerce",
+        href: "#",
+      },
+      {
+        title: "Microservices",
+        href: "#",
+      },
+      {
+        title: "Cloud & DevSecOps",
+        href: "#",
+      },
+      {
+        title: "Data Intelligence",
+        href: "#",
+      },
+      {
+        title: "Custom Development",
+        href: "#",
+      },
+      {
+        title: "Analytics Automation",
+        href: "",
+        para: "Be a partner for industry verticals on the inevitable journey towards enterprise.",
+      },
+    ];
     return (
       <>
         <section className={styles.subsection}>
@@ -29,12 +60,137 @@ const Header = () => {
             <h1>Latest from technology</h1>
             <div className={styles.imageTech}>
               <img src="/images/img/nav-img.png" alt="nav-image" />
+              <div className={styles.updateDiv}>
+                <div>
+                  <p className={styles.updatePara}>
+                    Learn more about the latest Drupal Updates
+                  </p>
+                </div>
+                <div>
+                  <Icons.ArrowRight size={18} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.nextHref}>
+              <div>
+                <p className={styles.updatePara1}>
+                  CMS has been the game changer of 2023 website trends
+                </p>
+              </div>
+              <div>
+                <Icons.ArrowRight size={18} />
+              </div>
             </div>
           </div>
-          <div className={styles.overviewTech}>ffsfsdfsd</div>
+
+          <div className={styles.overviewTech}>
+            <div className={styles.overviewFlex}>
+              <div className={styles.overviewSubFlex}>
+                <div>
+                  <img src="/images/icons/headerIcon.png" alt="setting-icon" />
+                </div>
+                <div className={styles.overviewTitle}>
+                  <h1>Technology Overview</h1>
+                  <p>
+                    Be a partner for industry verticals on the inevitable
+                    <br />
+                    journey towards enterprise.
+                  </p>
+                </div>
+              </div>
+              <div className={styles.overviewBtn}>
+                <Button variant="outline" size="sm">
+                  Overview <Icons.ArrowRight size={18} />
+                </Button>
+              </div>
+            </div>
+            <div className={styles.multiHrefs}>
+              {multiredirection.map((key) => {
+                return (
+                  <>
+                    <div className={styles.hrefsFlex}>
+                      <div className={styles.hrefInnerFlex}>
+                        <h1>{key.title}</h1>
+                        <Icons.ArrowForward />
+                      </div>
+                      {key.para && (
+                        <p className={styles.excepPara}>{key.para}</p>
+                      )}
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </section>
       </>
     );
+  };
+
+  const renderMoreItemsMenu = () => {
+    const multiMoreRedirection = [
+      {
+        title: "CMS",
+        href: "#",
+      },
+      {
+        title: "Commerce",
+        href: "#",
+      },
+      {
+        title: "Microservices",
+        href: "#",
+      },
+      {
+        title: "Cloud & DevSecOps",
+        href: "#",
+      },
+    ];
+    <>
+      <section className={styles.subsection}>
+        <div className={styles.latestTech}>
+          <h1>Latest from technology</h1>
+          <div className={styles.imageTech}>
+            <img src="/images/img/nav-img.png" alt="nav-image" />
+            <div className={styles.updateDiv}>
+              <div>
+                <p className={styles.updatePara}>
+                  Learn more about the latest Drupal Updates
+                </p>
+              </div>
+              <div>
+                <Icons.ArrowRight size={18} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.nextHref}>
+            <div>
+              <p className={styles.updatePara1}>
+                CMS has been the game changer of 2023 website trends
+              </p>
+            </div>
+            <div>
+              <Icons.ArrowRight size={18} />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.overviewTech}>
+          {multiMoreRedirection.map((key) => {
+            return (
+              <>
+                <div className={styles.hrefsFlex}>
+                  <div className={styles.hrefInnerFlex}>
+                    <h1>{key.title}</h1>
+                    <Icons.ArrowForward />
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </section>
+    </>;
   };
 
   return (
@@ -93,20 +249,7 @@ const Header = () => {
             <Link href="/">
               <Icons.Moredot width={4} height={14} /> More
             </Link>
-            <ul className={styles.subMenu}>
-              <li>
-                <Link href="/technology/cms">Roadmap</Link>
-              </li>
-              <li>
-                <Link href="/">FAQ</Link>
-              </li>
-              <li>
-                <Link href="/">Release</Link>
-              </li>
-              <li>
-                <Link href="/">Careers</Link>
-              </li>
-            </ul>
+            <ul className={styles.subMenu}>{renderMoreItemsMenu()}</ul>
           </li>
         </ul>
       </nav>
