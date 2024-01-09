9693dc98b77a75c83074d8ec6cd6f0bcbe14847f
sachinsharma92 <sachindesigner2018@gmail.com>
2024-01-09 13:37:15 +0530

    FIX: major issues

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     domains: ["plus.unsplash.com", "images.unsplash.com"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
