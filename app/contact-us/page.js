7f66a095cd5f48d98867ab7494d9b8cd7bf31f38
Jateen Sunder <jateen.sunder@techchefz.com>
2024-01-16 11:35:55 +0530

    working on contact us

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,12 +1,12 @@
 import ContactHeroBanner from "@/components/contactUs/contactHeroBanner";
-import DistinctLocations from "@/components/contactUs/distinctLocations";
 
 
 const ContactUs = () => {
   return (
     <div>
       <ContactHeroBanner />
-      <DistinctLocations />
+      {/* <LetsTalk />
+      <DistinctLocations /> */}
     </div>
   );
 };
