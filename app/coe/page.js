a791935171aeab7c4d66741830262b247962542e
sachinsharma92 <sachindesigner2018@gmail.com>
2024-01-19 11:01:20 +0530

    FIX: some images load issues

--- app/coe/page.js
+++ app/coe/page.js
@@ -1,22 +1,22 @@
-import OurValues from '@/components/about/ourValues'
-import Impact from '@/components/accelerators/acceleratorDetail/impact'
-import Challenges from '@/components/caseStudy/challenges'
-import CoeBanner from '@/components/coe/coeBanner'
-import LetsWork from '@/components/home/letsWork'
-import Consistent from '@/components/technology/consistent'
-import React from 'react'
+import OurValues from "@/components/about/ourValues";
+import Impact from "@/components/accelerators/acceleratorDetail/impact";
+import Challenges from "@/components/caseStudy/challenges";
+import CoeBanner from "@/components/coe/coeBanner";
+import LetsWork from "@/components/home/letsWork";
+import Consistent from "@/components/technology/consistent";
+import React from "react";
 
 const Coe = () => {
   return (
     <div>
-        <CoeBanner />
-        <OurValues/>
-        <Challenges className="pt-5" />
-        <Impact/>
-        <Consistent/>
-        <LetsWork/>
+      <CoeBanner />
+      <OurValues />
+      <Challenges className="pt-5" />
+      <Impact />
+      <Consistent />
+      <LetsWork />
     </div>
-  )
-}
+  );
+};
 
-export default Coe
+export default Coe;
--- components/coe/coeBanner/index.jsx
+++ components/coe/coeBanner/index.jsx
@@ -1,32 +1,44 @@
-"use client"
+"use client";
 import React from "react";
 import { MyContext } from "@/context/theme";
 import { useContext } from "react";
-import { ImageCustom } from '@/components/ui/imageCustom'
+import { ImageCustom } from "@/components/ui/imageCustom";
 
-import styles from "./style.module.scss"
+import styles from "./style.module.scss";
 
 const CoeBanner = () => {
-   const { theme, setTheme } = useContext(MyContext);
+  const { theme, setTheme } = useContext(MyContext);
   return (
     <section className={`${styles.banner} ${theme ? styles.bannerDark : ""}`}>
-    <div className="primary-container">
-       <div className={styles.bannerContent}>
+      <div className="primary-container">
+        <div className={styles.bannerContent}>
           <h1 className={styles.bannerTitle}>
-          Techchefz’s Global<span>  Centre of Excellence</span>
+            Techchefz’s Global<span> Centre of Excellence</span>
           </h1>
           <div className={styles.bannerRight}>
-             <p>
-                Technology-driven solutions are becoming the norm in our industry. More than ever, customers expect us to assess and adopt innovation to optimise our performance and create more value. The Kent Global Centre of Excellence (CoE) helps keep us at the forefront of change in every strand of our business.
-             </p>
-             
+            <p>
+              Technology-driven solutions are becoming the norm in our industry.
+              More than ever, customers expect us to assess and adopt innovation
+              to optimize our performance and create more value. The Kent Global
+              Centre of Excellence (CoE) helps keep us at the forefront of
+              change in every strand of our business.
+            </p>
           </div>
-       </div>
+        </div>
 
-       <ImageCustom src={"/images/accelerators.png"} width={1360} height={400} alt="banner-img" />
-    </div>
- </section>
-  )
-}
+        <div className={styles.imgBox}>
+          <ImageCustom
+            src={
+              "https://images.unsplash.com/photo-1601933470096-0e34634ffcde?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
+            width={1920}
+            height={1080}
+            alt="banner-img"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default CoeBanner
+export default CoeBanner;
