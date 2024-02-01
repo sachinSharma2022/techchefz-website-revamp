8eeaf857223013cfbbc3082422c0fd14be529651
Khalid0102 <khalid.ansari@techchefz.com>
2024-02-01 16:15:10 +0530

    opacity added in section animation

--- components/ui/sectionAnimation/animation.js
+++ components/ui/sectionAnimation/animation.js
@@ -1,10 +1,12 @@
 export const slideUp={
     initial: {
         y: "50%",
+        opacity: 0,
       },
       enter: {
         y: "0%",
         transition: {duration: 0.5},
+        opacity: 1,
       },
       exit: {
         y: "0%",
