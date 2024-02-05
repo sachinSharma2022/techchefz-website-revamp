f513364369066df8f7dae03022437159371a59d3
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-05 12:17:16 +0530

    FIX: opacity

--- components/home/projects/index.jsx
+++ components/home/projects/index.jsx
@@ -124,33 +124,31 @@ const Projects = () => {
       }`}
     >
       <div className={cn("primary-container")}>
-     <TextRevel>
+        <TextRevel>
+          <div className={cn("row", styles.rowSection)}>
+            <div className="col-md-12 col-12">
+              <p className={styles.projectHighlight}>Projects</p>
+            </div>
+            <div className="col-md-5 col-12">
+              <h3 className={styles.datingText}>
+                Unveiling Spotlight on Select Case Studies.
+              </h3>
+            </div>
+            <div className="col-md-7 col-12">
+              <p className={styles.aboutText}>
+                Delve into our case studies to witness firsthand how we&apos;ve
+                tackled challenges, delivered solutions, and achieved measurable
+                success. Each story is a testament to our commitment, expertise,
+                and the transformative impact.
+              </p>
 
-     
-        <div className={cn("row", styles.rowSection)}>
-          <div className="col-md-12 col-12">
-            <p className={styles.projectHighlight}>Projects</p>
+              <Link href="/portfolio">
+                <Button variant="outline" size="md">
+                  Check Our Portfolio <Icons.ArrowRight size={18} />
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className="col-md-5 col-12">
-            <h3 className={styles.datingText}>
-              Unveiling Spotlight on Select Case Studies.
-            </h3>
-          </div>
-          <div className="col-md-7 col-12">
-            <p className={styles.aboutText}>
-              Delve into our case studies to witness firsthand how we&apos;ve
-              tackled challenges, delivered solutions, and achieved measurable
-              success. Each story is a testament to our commitment, expertise,
-              and the transformative impact.
-            </p>
-
-            <Link href="/portfolio">
-              <Button variant="outline" size="md">
-                Check Our Portfolio <Icons.ArrowRight size={18} />
-              </Button>
-            </Link>
-          </div>
-        </div>
         </TextRevel>
         <div ref={container} className={styles.cards}>
           {projects.map((project, i) => {
