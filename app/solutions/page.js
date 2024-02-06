dbb920e1b6222e472dad17aa26f420713150d311
Khalid0102 <khalid.ansari@techchefz.com>
2024-02-06 14:24:00 +0530

    smooth scroll added

--- app/solutions/page.js
+++ app/solutions/page.js
@@ -1,4 +1,4 @@
-'use client'
+
 import Faq from "@/components/common/faq";
 import RelatedCase from "@/components/relatedCase";
 import SolutionHeroBanner from "@/components/solutions/solutionHeroBanner";
@@ -8,28 +8,10 @@ import SolutionTheProcess from "@/components/solutions/solutionTheProcess";
 
 import styles from "./style.module.scss";
 import SolutionCaseStudy from "@/components/solutions/solutionCaseStudy";
-import Lenis from '@studio-freight/lenis'
-import {useEffect} from 'react'
+
 
 const Solutions = () => {
-  useEffect( () => {
-    
-    (
-
-      async () => {
-
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-
-          const locomotiveScroll = new LocomotiveScroll({
-            lenisOptions: {duration: 0.2,}});
-
-      }
-
-    )()
-
-   
-
-  }, [])
+ 
   return (
     <div className={styles.solutionsPageStyle}>
       <SolutionHeroBanner />
