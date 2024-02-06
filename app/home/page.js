9f2449afee0f59364e9e70ec05d833c3d5c301c2
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-06 16:51:20 +0530

    FIX: extra code

--- app/home/page.js
+++ app/home/page.js
@@ -1,4 +1,3 @@
-'use client'
 import DigitalTransformation from "@/components/home/digitalTransformation";
 import HomeTestimonials from "@/components/home/homeTestimonials";
 import LandingBanner from "@/components/home/landingBanner";
@@ -10,34 +9,11 @@ import TechnologyStack from "@/components/home/technologyStack";
 import WeAreFuture from "@/components/home/weAreFuture";
 import Curve from "@/components/ui/pageTransition";
 import VideoCustom from "@/components/ui/videoCustom";
-import Lenis from '@studio-freight/lenis'
-import {useEffect} from 'react'
 
 const HomePage = () => {
-
-  useEffect( () => {
-
-    
-
-
-    const lenis = new Lenis({
-      duration: .7,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      infinite: false,
-  });
-  
-  function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-  }
-  
-  requestAnimationFrame(raf);
-
-  }, [])
   return (
     <>
       <Curve>
-
         <LandingBanner />
         <VideoCustom src="global.mp4" />
         <WeAreFuture />
