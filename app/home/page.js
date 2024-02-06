83cb70cde70271b6518f0c06bf0ee73de250f5c8
Manoj <manojindyaa@gmail.com>
2024-02-06 17:15:43 +0530

    integration changes

--- app/home/page.js
+++ app/home/page.js
@@ -1,4 +1,3 @@
-
 import DigitalTransformation from "@/components/home/digitalTransformation";
 import HomeTestimonials from "@/components/home/homeTestimonials";
 import LandingBanner from "@/components/home/landingBanner";
@@ -10,31 +9,44 @@ import TechnologyStack from "@/components/home/technologyStack";
 import WeAreFuture from "@/components/home/weAreFuture";
 import Curve from "@/components/ui/pageTransition";
 import VideoCustom from "@/components/ui/videoCustom";
-import { api_Home_Page } from "@/lib/constants";
-import { getData } from "@/lib/fetchData";
+import Lenis from '@studio-freight/lenis'
+import {useEffect} from 'react'
+
+const HomePage = () => {
+
+  useEffect( () => {
+
+    
 
 
-const HomePage = async () => {
-  const data = await getData(api_Home_Page);
+    const lenis = new Lenis({
+      duration: .7,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      infinite: false,
+  });
+  
+  function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+
+  }, [])
   return (
     <>
       <Curve>
-        {data ? (
-          <div>
-            <LandingBanner props={data.Banner} />
-            <VideoCustom src="global.mp4" />
-            <WeAreFuture props={data.Technology} />
-            <Service props={data.Services} />
-            <Projects project={data.Project} brands={data.Brands} />
-            <OurNumbers carrer={data.carrer} experience={data.ourExperience} />
-            <TechnologyStack technology={data.Technologys} />
-            <DigitalTransformation digital={data.digitalTransform} />
-            <HomeTestimonials testimonials={data.Testimonials} />
-            <LetsWork contact={data.ContactUs} />
-          </div>
-        ) : (
-          <>{/* {"API fail fallback"} */}</>
-        )}
+
+        <LandingBanner />
+        <VideoCustom src="global.mp4" />
+        <WeAreFuture />
+        <Service />
+        <Projects />
+        <OurNumbers />
+        <TechnologyStack />
+        <DigitalTransformation />
+        <HomeTestimonials />
+        <LetsWork />
       </Curve>
     </>
   );
