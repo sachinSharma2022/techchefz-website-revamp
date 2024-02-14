8b27ff4609162d2c546104aa47d7057042eae8c5
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-14 11:07:34 +0530

    FIX: ips

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["plus.unsplash.com", "images.unsplash.com", "127.0.0.1"],
+    domains: ["plus.unsplash.com", "images.unsplash.com", "122.176.75.250"],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   env: {
-    NODEMAILER_FROM_EMAIL: 'manoj.yadav@techchefz.com',
-    NODEMAILER_TO_EMAIL: 'khalid406090@gmail.com',
-    NODEMAILER_PW:"ucpw vzze prbs wooi"
+    NODEMAILER_FROM_EMAIL: "manoj.yadav@techchefz.com",
+    NODEMAILER_TO_EMAIL: "khalid406090@gmail.com",
+    NODEMAILER_PW: "ucpw vzze prbs wooi",
   },
 };
 
