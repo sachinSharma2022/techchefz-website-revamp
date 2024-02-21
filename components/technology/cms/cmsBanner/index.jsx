fb077580934f6ba5004c5663e2aeb73f32511efd
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-21 11:18:40 +0530

    FIX: CMS buttons

--- components/technology/cms/cmsBanner/index.jsx
+++ components/technology/cms/cmsBanner/index.jsx
@@ -5,6 +5,9 @@ import { useContext } from "react";
 import { cn } from "@/lib/utils";
 
 import styles from "./style.module.scss";
+import Link from "next/link";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
 
 const CmsBanner = ({ props }) => {
   const { theme, setTheme } = useContext(MyContext);
@@ -19,10 +22,22 @@ const CmsBanner = ({ props }) => {
           className={cn(styles.title, "gradient-text")}
           dangerouslySetInnerHTML={{ __html: `${props?.title}` }}
         ></h1>
-        <p
-          className={cn(styles.leadText, "gradient-text")}
-          dangerouslySetInnerHTML={{ __html: `${props.subtitle}` }}
-        ></p>
+        <div className={styles.contentSec}>
+          <p
+            className={cn(styles.leadText, "gradient-text")}
+            dangerouslySetInnerHTML={{ __html: `${props.subtitle}` }}
+          ></p>
+          <Link href="/contact-us">
+            <Button
+              variant={theme ? "blueBtnDark" : "blueBtn"}
+              className={styles.headerBtn}
+              size="md"
+            >
+              {props[0]?.Button}
+              <Icons.ArrowRight size={20} className="ms-2" />
+            </Button>
+          </Link>
+        </div>
       </div>
     </section>
   );
