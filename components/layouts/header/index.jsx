d1a783ba739d79089bdabc9a0b74bfe673d0e858
Manoj <manojindyaa@gmail.com>
2024-02-21 10:56:57 +0530

    code changes in all technology faq section up

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,17 +1,20 @@
-"use client";
+
 
 import { useMediaQuery } from "react-responsive";
 import NavigationDesktop from "../navigation/navigationDesktop";
 import NavigationMobile from "../navigation/navigationMobile";
+import { api_header_Page } from "@/lib/constants";
+import { getData } from "@/lib/fetchData";
 
-const Header = () => {
-  return (
+const Header = async () =>  {
+  const data = await getData(api_header_Page);
+    return (
     <>
       {/* For Mobile */}
       <NavigationMobile />
 
       {/* For Desktop */}
-      <NavigationDesktop />
+      <NavigationDesktop props={data?.HeaderMenu} />
     </>
   );
 };
