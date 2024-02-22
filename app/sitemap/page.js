5bc6abd471a2c7946a0c5ab4b92dfecbfff56d5e
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-22 14:24:48 +0530

    FIX: major issues

--- app/sitemap/page.js
+++ app/sitemap/page.js
@@ -4,13 +4,13 @@ import SiteMapLinks from "@/components/sitemap/siteMapLinks";
 import { api_sitemaps_Page } from "@/lib/constants";
 import { getData } from "@/lib/fetchData";
 
-const Sitemap = async() => {
-  const data = await getData(api_sitemaps_Page)
+const Sitemap = async () => {
+  const data = await getData(api_sitemaps_Page);
   return (
     <div>
-      <SiteMapBanner props={data.Banner}/>
-      <SiteMapLinks props={data.sitemaplink}/>
-      <LetsWork contact={data.LetsWork}/>
+      <SiteMapBanner props={data.Banner} />
+      <SiteMapLinks props={data.sitemaplink} />
+      <LetsWork contact={data.LetsWork} />
     </div>
   );
 };
--- components/sitemap/siteMapLinks/index.jsx
+++ components/sitemap/siteMapLinks/index.jsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import styles from "./style.module.scss";
 
-function SiteMapLinks({props}) {
-  console.log("sm",props);
+function SiteMapLinks({ props }) {
+  console.log("sm", props);
   const { theme } = useContext(MyContext);
   return (
     <section
@@ -16,26 +16,28 @@ function SiteMapLinks({props}) {
     >
       <div className={cn("primary-container", styles.flexContainer)}>
         <div className={styles.sitemapPage}>
-        {props?.tags1?.map((data, index) => (
-        <Link href= {data?.Links}>
-          <h6 dangerouslySetInnerHTML={{ __html: `${data?.Title}`}}></h6>
-        </Link>
-         ))}
-         
+          {props?.tags1?.map((data, index) => (
+            <Link key={index} href={data?.Links}>
+              <h6 dangerouslySetInnerHTML={{ __html: `${data?.Title}` }}></h6>
+            </Link>
+          ))}
         </div>
         <div className={styles.sitemapContent}>
-        <Link href= {props?.Link2}>
-          <h6 dangerouslySetInnerHTML={{ __html: `${props?.Title2}`}}></h6></Link>
+          <Link href={props?.Link2}>
+            <h6 dangerouslySetInnerHTML={{ __html: `${props?.Title2}` }}></h6>
+          </Link>
           {props?.tags2?.map((data, index) => (
-          <Link href= {data?.Links}>
-          <p dangerouslySetInnerHTML={{ __html: `${data?.Title}`}}></p></Link>
-           ))}
+            <Link key={index} href={data?.Links}>
+              <p dangerouslySetInnerHTML={{ __html: `${data?.Title}` }}></p>
+            </Link>
+          ))}
         </div>
         <div className={styles.sitemapContent}>
-          <h6 dangerouslySetInnerHTML={{ __html: `${props?.Title3}`}}></h6>
+          <h6 dangerouslySetInnerHTML={{ __html: `${props?.Title3}` }}></h6>
           {props?.tags3?.map((data, index) => (
-          <Link href= {data?.Links}>
-          <p dangerouslySetInnerHTML={{ __html: `${data?.Title}`}}></p></Link>
+            <Link key={index} href={data?.Links}>
+              <p dangerouslySetInnerHTML={{ __html: `${data?.Title}` }}></p>
+            </Link>
           ))}
         </div>
       </div>
