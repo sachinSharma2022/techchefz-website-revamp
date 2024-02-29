a1c6d3e3639cf5fcea2e2104eee0bfe2ba9f9243
Jateen Sunder <jateen.sunder@techchefz.com>
2024-02-29 18:33:53 +0530

    Ui fixes

--- components/solutions/solutionOurValues/index.jsx
+++ components/solutions/solutionOurValues/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Disclosure,Transition } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 import { useState } from "react";
 import { MyContext } from "@/context/theme";
 import { useContext } from "react";
@@ -9,13 +9,11 @@ import { cn } from "@/lib/utils";
 import { base_Uri } from "@/lib/constants";
 import styles from "./style.module.scss";
 import { base_Url } from "@/lib/constants";
-import { motion ,AnimatePresence} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const SolutionOurValues = ({ props }) => {
   const [activeDisclosurePanel, setActiveDisclosurePanel] = useState(null);
   const { theme, setTheme } = useContext(MyContext);
-
-
 
   function togglePanels(newPanel) {
     if (activeDisclosurePanel) {
@@ -82,24 +80,35 @@ const SolutionOurValues = ({ props }) => {
                       />
                       <h4 className={styles.title}>{data.Title}</h4>
                       <div className={styles.iconBox}>
-                      <motion.span
-              initial={{ backgroundColor: "transparent" }}
-              animate={{ rotate: open ? 180 : 0 }}
-              transition={{
-                duration: 0.15,
-                type: "tween",
-              }}>
-              <Icons.ArrowDown /> 
-              </motion.span>
+                        <motion.span
+                          initial={{ backgroundColor: "transparent" }}
+                          animate={{ rotate: open ? 180 : 0 }}
+                          transition={{
+                            duration: 0.15,
+                            type: "tween",
+                          }}
+                        >
+                          <Icons.ArrowDown />
+                        </motion.span>
                       </div>
                     </Disclosure.Button>
                     <AnimatePresence>
-                    <Disclosure.Panel as={motion.div} initial={{ y: -20, opacity: 0.2 }}
-    animate={{ y: 0, opacity: 1, }} transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }} exit={{
-       y: -20, opacity: 0.2 ,
-      transition: { duration: 0.4 },
-    }} className={styles.accordionBody}>
-                      <div className={styles.imgBox}>
+                      <Disclosure.Panel
+                        as={motion.div}
+                        initial={{ y: -20, opacity: 0.2 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                          duration: 0.4,
+                          ease: [0.04, 0.62, 0.23, 0.98],
+                        }}
+                        exit={{
+                          y: -20,
+                          opacity: 0.2,
+                          transition: { duration: 0.4 },
+                        }}
+                        className={styles.accordionBody}
+                      >
+                        {/* <div className={styles.imgBox}>
                         <ImageCustom
                           src={
                             data.img.data.attributes.url
@@ -110,24 +119,22 @@ const SolutionOurValues = ({ props }) => {
                           height={200}
                           alt="bannerImg"
                         />
-                      </div> 
-                      <div className={styles.accordionBodyContent}>
-                        {data.Description}
-                        <ul className={styles.listStyle}>
-                          {data?.secoundDescription
-                            ?.split(",")
-                            .map((items, index) => (
-                              <li key={index}>
-                                {/* <Icons.CheckIcon /> */}
-                                {items}
-                              </li>
-                            ))}
-                        </ul>
-                      </div>
-                    </Disclosure.Panel>
+                      </div>  */}
+                        <div className={styles.accordionBodyContent}>
+                          {data.Description}
+                          <ul className={styles.listStyle}>
+                            {data?.secoundDescription
+                              ?.split(",")
+                              .map((items, index) => (
+                                <li key={index}>
+                                  {/* <Icons.CheckIcon /> */}
+                                  {items}
+                                </li>
+                              ))}
+                          </ul>
+                        </div>
+                      </Disclosure.Panel>
                     </AnimatePresence>
-                    
-                   
                   </>
                 );
               }}
