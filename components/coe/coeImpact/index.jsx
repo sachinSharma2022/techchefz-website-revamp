b01980a460671d3e8cf75e52fcfa64426ea9208a
sachinsharma92 <sachindesigner2018@gmail.com>
2024-02-29 14:30:28 +0530

    FIX: major issues

--- components/coe/coeImpact/index.jsx
+++ components/coe/coeImpact/index.jsx
@@ -7,64 +7,78 @@ import { base_Uri } from "@/lib/constants";
 import styles from "./style.module.scss";
 import { cn } from "@/lib/utils";
 
-const CoeImpact = ({props}) => {
+const CoeImpact = ({ props }) => {
   const { theme, setTheme } = useContext(MyContext);
   return (
     <section className={`${styles.impact} ${theme ? styles.impactDark : ""}`}>
       <div className={cn("primary-container")}>
         <div className={styles.headSection}>
-          <h6 className={cn(styles.subHeading,"gradient-text")} dangerouslySetInnerHTML={{ __html: `${props?.Title}`}}>
-          </h6>
-          <h3 className={cn(styles.impactHeading,"gradient-text")} dangerouslySetInnerHTML={{ __html: `${props?.SubTitle}`}}>
-          </h3>
+          <h6
+            className={cn(styles.subHeading, "gradient-text")}
+            dangerouslySetInnerHTML={{ __html: `${props?.Title}` }}
+          ></h6>
+          <h3
+            className={cn(styles.impactHeading, "gradient-text")}
+            dangerouslySetInnerHTML={{ __html: `${props?.SubTitle}` }}
+          ></h3>
         </div>
 
-        <div className={styles.impactTopSection}>
-          <div className={styles.impactImage}>
-            <ImageCustom
-              src={props?.TechnologicalCard[0].Image.data.attributes.url?`${base_Uri}${props?.TechnologicalCard[0].Image.data.attributes.url}`:`${base_Uri}/`}
-              width={700}
-              height={600}
-              alt="impact-img"
-            />
+        <div className={styles.cardSection}>
+          <div className={styles.impactTopSection}>
+            <div className={styles.impactImage}>
+              <ImageCustom
+                src={
+                  props?.TechnologicalCard[0].Image.data.attributes.url
+                    ? `${base_Uri}${props?.TechnologicalCard[0].Image.data.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={700}
+                height={600}
+                alt="impact-img"
+              />
+            </div>
+            <div className={styles.topSectionRight}>
+              <h4>{props?.TechnologicalCard[0]?.Title}</h4>
+              <p>{props?.TechnologicalCard[0]?.Description}</p>
+            </div>
           </div>
-          <div className={styles.topSectionRight}>
-            <h4>{props?.TechnologicalCard[0]?.Title}</h4>
-            <p>
-            {props?.TechnologicalCard[0]?.Description}
-            </p>
+
+          <div className={styles.impactTopSection}>
+            <div className={styles.topSectionRight}>
+              <h4>{props?.TechnologicalCard[1]?.Title}</h4>
+              <p>{props?.TechnologicalCard[1]?.Description}</p>
+            </div>
+            <div className={styles.impactImage}>
+              <ImageCustom
+                src={
+                  props?.TechnologicalCard[1].Image.data.attributes.url
+                    ? `${base_Uri}${props?.TechnologicalCard[1].Image.data.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={700}
+                height={600}
+                alt="impact-img"
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.impactTopSection}>
-          <div className={styles.topSectionRight}>
-            <h4>{props?.TechnologicalCard[1]?.Title}</h4>
-            <p>
-            {props?.TechnologicalCard[1]?.Description}
-            </p>
-          </div>
-          <div className={styles.impactImage}>
-            <ImageCustom
-             src={props?.TechnologicalCard[1].Image.data.attributes.url?`${base_Uri}${props?.TechnologicalCard[1].Image.data.attributes.url}`:`${base_Uri}/`}
-              width={700}
-              height={600}
-              alt="impact-img"
-            />
-          </div>
-        </div>
-        <div className={styles.impactTopSection}>
-          <div className={styles.impactImage}>
-            <ImageCustom
-              src={props?.TechnologicalCard[2].Image.data.attributes.url?`${base_Uri}${props?.TechnologicalCard[2].Image.data.attributes.url}`:`${base_Uri}/`}
-              width={700}
-              height={600}
-              alt="impact-img"
-            />
-          </div>
-          <div className={styles.topSectionRight}>
-            <h4>{props?.TechnologicalCard[2]?.Title}</h4>
-            <p>
-            {props?.TechnologicalCard[2]?.Description}
-            </p>
+
+          <div className={styles.impactTopSection}>
+            <div className={styles.impactImage}>
+              <ImageCustom
+                src={
+                  props?.TechnologicalCard[2].Image.data.attributes.url
+                    ? `${base_Uri}${props?.TechnologicalCard[2].Image.data.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={700}
+                height={600}
+                alt="impact-img"
+              />
+            </div>
+            <div className={styles.topSectionRight}>
+              <h4>{props?.TechnologicalCard[2]?.Title}</h4>
+              <p>{props?.TechnologicalCard[2]?.Description}</p>
+            </div>
           </div>
         </div>
       </div>
