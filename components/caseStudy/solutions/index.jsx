40900dc2f58aadde7197861933c5d00a14e4e8f6
Manoj <manojindyaa@gmail.com>
2024-03-04 17:11:54 +0530

    image portfolio bug fixes

--- components/caseStudy/solutions/index.jsx
+++ components/caseStudy/solutions/index.jsx
@@ -34,17 +34,20 @@ const Solutions = ({ props }) => {
               {props.Views.map((data, index) => (
                 <>
                   <div key={index} className={styles.goalsTextWrapper}>
-                    <div className={styles.goalsIcons}>
-                      <ImageCustom
-                        src={
-                          data?.Image?.data?.attributes?.url
-                            ? `${base_Url}${data?.Image?.data?.attributes?.url}`
-                            : `${base_Url}/`
-                        }
-                        width={40}
-                        height={28}
-                      />
-                    </div>
+                    {data?.Image?.data?.attributes?.url !== undefined &&
+                      data?.Image?.data?.attributes?.url !== "" && (
+                        <div className={styles.goalsIcons}>
+                          <ImageCustom
+                            src={
+                              data?.Image?.data?.attributes?.url
+                                ? `${base_Url}${data?.Image?.data?.attributes?.url}`
+                                : `${base_Url}/`
+                            }
+                            width={40}
+                            height={28}
+                          />
+                        </div>
+                      )}
                     <p className={styles.goalsText}>{data.Title}</p>
                   </div>
                 </>
@@ -52,42 +55,48 @@ const Solutions = ({ props }) => {
             </div>
           </div>
         </div>
-
-        <div className={styles.solutionImages}>
-          <ImageCustom
-            src={
-              props?.gallaryImages[0].Image?.data?.attributes?.url
-                ? `${base_Uri}${props?.gallaryImages[0].Image.data?.attributes.url}`
-                : `${base_Uri}/`
-            }
-            width={1060}
-            height={500}
-            alt="content-img"
-            className={styles.goalInnerImage}
-          />
-          <ImageCustom
-            src={
-              props?.gallaryImages[1].Image.data?.attributes.url
-                ? `${base_Uri}${props?.gallaryImages[1].Image.data?.attributes.url}`
-                : `${base_Uri}/`
-            }
-            width={500}
-            height={400}
-            alt="content-img"
-            className={styles.goalInnerImage}
-          />
-          <ImageCustom
-            src={
-              props?.gallaryImages[2].Image.data?.attributes.url
-                ? `${base_Uri}${props?.gallaryImages[2].Image.data?.attributes.url}`
-                : `${base_Uri}/`
-            }
-            width={500}
-            height={400}
-            alt="content-img"
-            className={styles.goalInnerImage}
-          />
-        </div>
+        {props?.gallaryImages[0]?.Image?.data?.attributes?.url !== undefined &&
+          props?.gallaryImages[0]?.Image?.data?.attributes?.url !== "" &&
+          props?.gallaryImages[1]?.Image.data?.attributes.url !== undefined &&
+          props?.gallaryImages[1]?.Image.data?.attributes.url !== "" &&
+          props?.gallaryImages[2]?.Image.data?.attributes.url !== undefined &&
+          props?.gallaryImages[2]?.Image.data?.attributes.url !== "" && (
+            <div className={styles.solutionImages}>
+              <ImageCustom
+                src={
+                  props?.gallaryImages[0].Image?.data?.attributes?.url
+                    ? `${base_Uri}${props?.gallaryImages[0].Image.data?.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={1060}
+                height={500}
+                alt="content-img"
+                className={styles.goalInnerImage}
+              />
+              <ImageCustom
+                src={
+                  props?.gallaryImages[1].Image.data?.attributes.url
+                    ? `${base_Uri}${props?.gallaryImages[1].Image.data?.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={500}
+                height={400}
+                alt="content-img"
+                className={styles.goalInnerImage}
+              />
+              <ImageCustom
+                src={
+                  props?.gallaryImages[2].Image.data?.attributes.url
+                    ? `${base_Uri}${props?.gallaryImages[2].Image.data?.attributes.url}`
+                    : `${base_Uri}/`
+                }
+                width={500}
+                height={400}
+                alt="content-img"
+                className={styles.goalInnerImage}
+              />
+            </div>
+          )}
       </div>
     </section>
   );
