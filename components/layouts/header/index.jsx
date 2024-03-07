48910acb73acbeadc432865b64483f496f973927
Manoj <manojindyaa@gmail.com>
2024-03-07 14:37:57 +0530

    Header blog dynemics integration

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,18 +1,19 @@
 import { useMediaQuery } from "react-responsive";
 import NavigationDesktop from "../navigation/navigationDesktop";
 import NavigationMobile from "../navigation/navigationMobile";
-import { api_header_Page } from "@/lib/constants";
+import { api_header_Page, api_insight_insides_Page } from "@/lib/constants";
 import { getDataDynamic } from "@/lib/fetchData";
 
 const Header = async () => {
   const data = await getDataDynamic(api_header_Page);
+  const data_inside = await getDataDynamic(api_insight_insides_Page);
   return (
     <>
       {/* For Mobile */}
       <NavigationMobile props={data?.attributes?.HeaderMenu} />
 
       {/* For Desktop */}
-      <NavigationDesktop props={data?.attributes?.HeaderMenu} />
+      <NavigationDesktop props={data?.attributes?.HeaderMenu} featureArticle={data_inside} />
     </>
   );
 };
