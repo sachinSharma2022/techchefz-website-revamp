03b6a4165a80642790a64376049527321badbb7b
Manoj <manojindyaa@gmail.com>
2024-03-07 16:42:30 +0530

    header bux solved

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,10 +1,13 @@
-import { api_header_Page } from "@/lib/constants";
-import { getDataDynamic } from "@/lib/fetchData";
+
 import NavigationDesktop from "../navigation/navigationDesktop";
 import NavigationMobile from "../navigation/navigationMobile";
 
+import { api_header_Page, api_insight_insides_Page } from "@/lib/constants";
+import { getDataDynamic } from "@/lib/fetchData";
+
 const Header = async () => {
   const data = await getDataDynamic(api_header_Page);
+  const data_inside = await getDataDynamic(api_insight_insides_Page);
   return (
     <>
       {data ? (
