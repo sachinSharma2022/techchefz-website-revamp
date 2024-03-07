e2b5b1f900d6ea5e107595a80f888b9bc0dcb3e2
sachinsharma92 <sachindesigner2018@gmail.com>
2024-03-07 14:49:13 +0530

    FIX: major UI issues

--- components/common/faq/index.jsx
+++ components/common/faq/index.jsx
@@ -2,14 +2,14 @@
 import { MyContext } from "@/context/theme";
 import { useContext } from "react";
 import { Icons } from "@/components/icons";
-import { Disclosure,Transition } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 import { useState } from "react";
 import styles from "./style.module.scss";
 import { cn } from "@/lib/utils";
-import { motion ,AnimatePresence} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import TextRevel from "@/components/ui/sectionAnimation";
 
-const Faq = ({props}) => {
+const Faq = ({ props }) => {
   const [activeDisclosurePanel, setActiveDisclosurePanel] = useState(null);
   const { theme, setTheme } = useContext(MyContext);
 
@@ -29,7 +29,6 @@ const Faq = ({props}) => {
     });
   }
 
-  
   return (
     <section
       className={cn(
@@ -39,30 +38,31 @@ const Faq = ({props}) => {
     >
       <div className={cn("primary-container")}>
         <TextRevel>
-        <div className={styles.faqHead}>
-          <h6 className={cn(styles.projectHighlight,"gradient-text")} dangerouslySetInnerHTML={{ __html: `${props?.Title}`}}>
-            </h6>
-          <h4 className={cn(styles.title,"gradient-text")} dangerouslySetInnerHTML={{ __html: `${props?.Description}`}}>
-            </h4>
-        </div>
+          <div className={styles.faqHead}>
+            <h6
+              className={cn(styles.projectHighlight, "gradient-text")}
+              dangerouslySetInnerHTML={{ __html: `${props?.Title}` }}
+            ></h6>
+            <h4
+              className={cn(styles.title, "gradient-text")}
+              dangerouslySetInnerHTML={{ __html: `${props?.Description}` }}
+            ></h4>
+          </div>
         </TextRevel>
-       
 
         <div className={styles.faqValuesContent}>
           <div className={styles.faqAccordion}>
-          {props?.innerFaqs?.map((data, index) => (
+            {props?.innerFaqs?.map((data, index) => (
               <Disclosure key={index}>
                 {(panel) => {
                   const { open, close } = panel;
                   return (
-                   
-                           <div  className={styles.faqSecMain}>
+                    <div className={styles.faqSecMain}>
                       <div className={styles.numberSec}>
                         <h2 className={styles.faqNumber}>{data.Title}</h2>
                       </div>
 
-                      <div  className={styles.faqList}>
-                        
+                      <div className={styles.faqList}>
                         <Disclosure.Button
                           className={styles.accordionHead}
                           onClick={() => {
@@ -72,23 +72,30 @@ const Faq = ({props}) => {
                             togglePanels({ ...panel, key: index });
                           }}
                         >
-                          {data.title}{data.Description}
+                          {data.title}
+                          {data.Description}
                           {!open ? <Icons.IconPlus /> : <Icons.IconMinus />}
                         </Disclosure.Button>
-                       
-                        <Disclosure.Panel as={motion.div} initial={{ y: -20, opacity: 0.2 }}
-    animate={{ y: 0, opacity: 1, }} transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }} exit={{
-       y: -20, opacity: 0.2 ,
-      transition: { duration: 0.4 },
-    }} className={styles.accordionBody}>
-                        {data.FullDescription}
+
+                        <Disclosure.Panel
+                          as={motion.div}
+                          initial={{ y: -20, opacity: 0.2 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.4,
+                            ease: [0.04, 0.62, 0.23, 0.98],
+                          }}
+                          exit={{
+                            y: -20,
+                            opacity: 0.2,
+                            transition: { duration: 0.4 },
+                          }}
+                          className={styles.accordionBody}
+                        >
+                          {data.FullDescription}
                         </Disclosure.Panel>
-                        
                       </div>
                     </div>
-                    
-
-                   
                   );
                 }}
               </Disclosure>
