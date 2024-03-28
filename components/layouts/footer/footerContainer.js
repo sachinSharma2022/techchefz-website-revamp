87eb19c806d434b91015ea778dd7dec1def58190
Khalid0102 <khalid.ansari@techchefz.com>
2024-03-28 11:24:15 +0530

    Footer is lazy loaded

--- components/layouts/footer/footerContainer.js
+++ components/layouts/footer/footerContainer.js
@@ -1,19 +1,15 @@
 import Footer from ".";
 import { getDataDynamic } from "@/lib/fetchData";
 import { api_footer_Page } from "@/lib/constants";
-import NotFound from "@/app/not-found";
+import FooterLazyLoad from "./footerLazyLoad";
 const FooterContainer = async () => {
   const data = await getDataDynamic(api_footer_Page);
 
   return (
     <>
       {data ? (
-        <Footer
-          props={data?.attributes?.Logo}
-          Subscribe={data?.attributes?.Subscribe}
-          SocialMedia={data?.attributes?.SocialMedia}
-          footerinner={data?.attributes?.footerinner}
-          Reserved={data?.attributes?.Reserved}
+        <FooterLazyLoad
+         data={data}
         />
       ) : (
         <></>
---
+++ components/layouts/footer/footerLazyLoad.js
@@ -0,0 +1,27 @@
+"use client";
+import { useRef } from "react";
+import dynamic from "next/dynamic";
+const Footer = dynamic(() => {
+  console.log("Footeradded");
+  return import("@/components/layouts/footer");
+});
+//import Footer from ".";
+import useLazyLoad from "@/lib/useLazyLoad";
+const FooterLazyLoad = ({ data }) => {
+  const targetRefFooter = useRef();
+  const isVisibleFooter = useLazyLoad(targetRefFooter);
+  return (
+    <div ref={targetRefFooter}>
+      {isVisibleFooter && (
+        <Footer
+          props={data?.attributes?.Logo}
+          Subscribe={data?.attributes?.Subscribe}
+          SocialMedia={data?.attributes?.SocialMedia}
+          footerinner={data?.attributes?.footerinner}
+          Reserved={data?.attributes?.Reserved}
+        />
+      )}
+    </div>
+  );
+};
+export default FooterLazyLoad;
