21d59aec9b2941768d581d2d0500d74273ae89d7
Khalid0102 <khalid.ansari@techchefz.com>
2024-04-01 17:21:22 +0530

    bug fix

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -3,7 +3,7 @@ export async function POST(request) {
   try {
     const text = await request.text();
     const obj = JSON.parse(text);
-    if (obj.model) revalidatePath(`/${obj.model}`, "page");
+    if (obj.model) revalidatePath(`/${obj.model}`, "layout");
   } catch (error) {
     return new Response(`Webhook error: ${error.message}`, {
       status: 400,
