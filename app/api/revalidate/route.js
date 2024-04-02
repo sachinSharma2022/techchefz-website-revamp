6867a1001ae5f41ce4d62f96de7c4ece8f257fa7
Khalid0102 <khalid.ansari@techchefz.com>
2024-04-02 12:37:18 +0530

    route alias added

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -3,7 +3,32 @@ export async function POST(request) {
   try {
     const text = await request.text();
     const obj = JSON.parse(text);
-    if (obj.model) revalidatePath(`/${obj.model}/`, "layout");
+    const alias = {
+      "landing-page": "home",
+      about: "about",
+      solution: "solutions",
+      technology: "technology",
+      "cms-technology": "technology/cms",
+      techcommerce: "technology/commerce",
+      "tech-microservice": "technology/microservices",
+      "tech-cloud-and-dev-sec-op": "technology/cloud-devsec-ops",
+      "data-intelligence": "technology/data-intelligence",
+      "custom-development": "technology/custom-development",
+      "analytics-automation": "technology/analytics-automation",
+      portfolio: "portfolio",
+      career: "careers",
+      insight: "insights",
+      coe: "coe",
+      "contact-us": "contact-us",
+      "privacy-policy": "privacy-policy",
+      sitemap: "sitemap",
+      accelerator: "",
+      "accelerators-detail": "",
+      "case-study": "portfolio",
+      "insight-inside": "insights",
+      "career-detail": "careers",
+    };
+    if (obj.model) revalidatePath(`/${alias[obj.model]}/`, "layout");
   } catch (error) {
     return new Response(`Webhook error: ${error.message}`, {
       status: 400,
