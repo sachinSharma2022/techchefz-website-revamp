27ed3d54c250762d3a0512e3bd0de85a556f7f23
Khalid0102 <khalid.ansari@techchefz.com>
2024-04-03 10:50:34 +0530

    marker removed

--- components/solutions/solutionTheProcess/index.jsx
+++ components/solutions/solutionTheProcess/index.jsx
@@ -106,7 +106,7 @@ const SolutionTheProcess = ({ props }) => {
               trigger: text,
               start: "top 390rem",
               end: "center 232rem",
-              markers: true,
+              markers: false,
               toggleActions: "play reset play reverse",
             },
             opacity: 1,
