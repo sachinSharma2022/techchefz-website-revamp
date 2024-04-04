1308251243c17bfd7d58b6b9a092b87509095b75
Khalid0102 <khalid.ansari@techchefz.com>
2024-04-04 16:50:15 +0530

    mergeconflcts issue fixed

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,12 @@
 import ThemeProvider from "@/context/theme";
 import { api_Home_Page } from "@/lib/constants";
 import { getData } from "@/lib/fetchData";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import "bootstrap/dist/css/bootstrap.css";
 //import Header from "../components/layouts/header";
 import { aeonik, helvetica } from "../lib/fonts";
 import { cn } from "../lib/utils";
 import "../styles/globals.scss";
 //import { GoogleAnalytics } from "@next/third-parties/google";
-import { api_Home_Page } from "@/lib/constants";
-import { getData } from "@/lib/fetchData";
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("@/components/layouts/header"));
 const FooterContainer = dynamic(() =>
--- components/layouts/footer/footerContainer.js
+++ components/layouts/footer/footerContainer.js
@@ -1,6 +1,6 @@
 import { api_footer_Page } from "@/lib/constants";
 import { getDataDynamic } from "@/lib/fetchData";
-import Footer from ".";
+import FooterLazyLoad from "./footerLazyLoad";
 export const FooterContainer = async () => {
   const data = await getDataDynamic(api_footer_Page);
 
