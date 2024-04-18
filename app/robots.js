07c600fbdd0ee90a02844f13e67fc1ace0513919
Pranav <pranav.kumar@techchefz.com>
2024-04-18 22:37:42 +0530

    config change for production

--- app/robots.js
+++ app/robots.js
@@ -2,7 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      disallow: "/",
+      disallow: "",
     },
     sitemap: `${process.env.NEXT_PUBLIC_SITEMAP_URL}/sitemap.xml`,
   };
