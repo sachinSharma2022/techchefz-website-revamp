869820cca33d16e674dbc1d1a594e1a63e290b1d
Pranav <pranav.kumar@techchefz.com>
2024-04-30 17:27:42 +0530

    title change

--- app/layout.js
+++ app/layout.js
@@ -21,10 +21,10 @@ export async function generateMetadata() {
   return {
     title: data?.SeoData?.Title,
     description: data?.SeoData?.Description,
+    name: "Techchefz Digital",
     openGraph: {
       title: data?.SeoData?.Title,
       description: data?.SeoData?.Description,
-      name: "Techchefz Digital",
       url: process.env.NEXT_PUBLIC_SITEMAP_URL,
       images: [
         {
