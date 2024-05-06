163b9d847c0eb0b7f831ff87ed5470fed66ce217
Khalid0102 <khalid.ansari@techchefz.com>
2024-05-06 13:48:15 +0530

    env variable changed

--- app/technology/page.js
+++ app/technology/page.js
@@ -33,11 +33,11 @@ const Technology = async () => {
     <>
       {data ? (
         <div>
-          <TechnologyBanner props={data.BannerComponent} />
+          {/* <TechnologyBanner props={data.BannerComponent} />
           <Streamline props={data.Extensive} />
-          <Partners props={data.PARTNERS} />
+          <Partners props={data.PARTNERS} /> */}
           {/* <Consistent props={data.Consistent} /> */}
-          <TechnologyValues
+          {/* <TechnologyValues
             wrapperStyle="technology-styles"
             props={data.Technology}
           />
@@ -49,7 +49,7 @@ const Technology = async () => {
             props={data_related_cases}
             RelatedInsight={data.RelatedInsight}
           />
-          <LetsWork contact={data.LetsWork} />
+          <LetsWork contact={data.LetsWork} /> */}
         </div>
       ) : (
         <>
