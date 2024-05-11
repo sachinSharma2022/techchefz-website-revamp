d16422b3794ef52cac1a90aae76d7961ba4f8050
Pranav <pranav.kumar@techchefz.com>
2024-05-11 13:10:49 +0530

    config change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
