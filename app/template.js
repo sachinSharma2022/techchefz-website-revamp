efc39dfa0533d82395e35796514b30dd578bc612
sachinsharma92 <sachindesigner2018@gmail.com>
2024-06-07 14:44:20 +0530

    REMOVE: commented code

--- app/template.js
+++ app/template.js
@@ -1,11 +1,6 @@
 "use client";
-import Curve from "@/components/ui/pageTransition";
-import { AnimatePresence } from "framer-motion";
 export default function rootTemplate({ children }) {
   return (
     <>{children}</>
-    // <AnimatePresence mode="wait">
-    //   <Curve>{children}</Curve>
-    // </AnimatePresence>
   );
 }
