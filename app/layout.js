084031f419a446b8387a3c6ad8f984c2456e246b
sachinsharma92 <sachindesigner2018@gmail.com>
2024-07-05 12:21:27 +0530

    Convert normal file to minify

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import ThemeProvider from "@/context/theme";
 import { api_Home_Page } from "@/lib/constants";
 import { getData } from "@/lib/fetchData";
-import "../styles/grid.css";
+import "../styles/grid.min.css";
 //import Header from "../components/layouts/header";
 import { aeonik, helvetica } from "../lib/fonts";
 import { cn } from "../lib/utils";
