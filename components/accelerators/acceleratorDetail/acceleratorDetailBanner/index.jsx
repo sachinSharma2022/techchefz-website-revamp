a62fb1f208a55d6d71c072e8b9fd710dfda75175
sachinsharma92 <sachindesigner2018@gmail.com>
2024-07-30 13:03:15 +0530

    FIX: container design issues

--- components/accelerators/acceleratorDetail/acceleratorDetailBanner/index.jsx
+++ components/accelerators/acceleratorDetail/acceleratorDetailBanner/index.jsx
@@ -13,7 +13,8 @@ import Link from "next/link";
 const DetailBanner = ({ props }) => {
   const { theme, setTheme } = useContext(MyContext);
   return (
-    <section className={`${styles.banner} ${theme ? styles.bannerDark : ""}`}>
+    <>
+    <section className={`${styles.banner} ${theme ? styles.bannerDark : ""} header-container`}>
       <div className="primary-container">
         <div className={cn(styles.bannerContent, "grid-col-2")}>
           <h1
@@ -36,22 +37,23 @@ const DetailBanner = ({ props }) => {
           </div>
         </div>
       </div>
-      {props?.Image?.data?.attributes?.url !== undefined &&
-        props?.Image?.data?.attributes?.url !== "" && (
-          <div className={styles.imgBox}>
-            <ImageCustom
-              src={
-                props?.Image?.data?.attributes?.url
-                  ? `${base_Uri}${props?.Image?.data?.attributes?.url}`
-                  : `${base_Uri}/`
-              }
-              width={1500}
-              height={1500}
-              alt={props?.Image?.data?.attributes?.alternativeText}
-            />
-          </div>
-        )}
     </section>
+    {props?.Image?.data?.attributes?.url !== undefined &&
+      props?.Image?.data?.attributes?.url !== "" && (
+        <div className={styles.imgBox}>
+          <ImageCustom
+            src={
+              props?.Image?.data?.attributes?.url
+                ? `${base_Uri}${props?.Image?.data?.attributes?.url}`
+                : `${base_Uri}/`
+            }
+            width={1500}
+            height={1500}
+            alt={props?.Image?.data?.attributes?.alternativeText}
+          />
+        </div>
+      )}
+      </>
   );
 };
 
