a4d5c0397dfefafa6201d8c8f40702075455df26
Pranav <pranav.kumar@techchefz.com>
2024-10-15 15:36:26 +0530

    emailer fix

--- next.config.js
+++ next.config.js
@@ -19,9 +19,9 @@ const nextConfig = {
   },
   env: {
     NODEMAILER_FROM_EMAIL: "contactus@techchefz.com",
-    NODEMAILER_TO_EMAIL: "sales@techchefz.com",
+    NODEMAILER_TO_EMAIL: "gaurav.chugh@techchefz.com,eshu.tripathi@techchefz.com,amit.thakral@techchefz.com",
     NODEMAILER_TO_JOB_EMAIL: "joinus@techchefz.com",
-    NODEMAILER_PW: "ovps czuv bujv wlwp",
+    NODEMAILER_PW: "axyk uvbs ilil uvug",
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6LevJ7ApAAAAAF5ZRMjbt3ii4S5ZPdrO-ht7vHi_",
     RECAPTCHA_SECRET_KEY: "6LevJ7ApAAAAAJGJYMiS4ozpdGAQ7h7rMeZW3oIm",
     NEXT_PUBLIC_GA_ID: "G-PFWD6G0FVF",
