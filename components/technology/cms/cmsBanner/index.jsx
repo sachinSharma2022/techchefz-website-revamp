3871b513ccc78b647beb0eeffb2f0c876e2c4ad3
Shivanand Prasad <64852426+shivanand2305@users.noreply.github.com>
2025-01-16 15:58:38 +0530

    fixed desc and btn

--- components/technology/cms/cmsBanner/index.jsx
+++ components/technology/cms/cmsBanner/index.jsx
@@ -27,7 +27,7 @@ const CmsBanner = ({ props }) => {
             <div className={styles.contentSec}>
               <p
                 className={cn(styles.leadText, "gradient-text")}
-                dangerouslySetInnerHTML={{ __html: `${props?.subtitle}` }}
+                dangerouslySetInnerHTML={{ __html: `${props?.description}` }}
               ></p>
               <Link href={props?.BtnLink}>
                 <Button
@@ -35,7 +35,7 @@ const CmsBanner = ({ props }) => {
                   className={styles.headerBtn}
                   size="md"
                 >
-                  {props?.Btn}
+                  {props?.button}
                   <Icons.ArrowRight size={20} className="ms-2" />
                 </Button>
               </Link>
